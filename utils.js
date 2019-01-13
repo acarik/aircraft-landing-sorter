d200317ab7b5515d1012b88d15c7e17cf1485ba6
acarik <acarik@gmail.com>
2019-01-13 21:45:22 +0300

    logging into log.txt

--- utils.js
+++ utils.js
@@ -30,11 +30,10 @@ function parseTimeStr(str){
     return time;
 }
 
-/*
 // file logger
 var fs = require('fs');
 var util = require('util');
-var logFile = fs.createWriteStream('consolelog.txt', { flags: 'w' });
+var logFile = fs.createWriteStream('log.txt', { flags: 'w' });
   // Or 'w' to truncate the file every time the process starts.
 var logStdout = process.stdout;
 
@@ -43,7 +42,8 @@ console.log = function () {
   logStdout.write(util.format.apply(null, arguments) + '\n');
 }
 console.error = console.log;
-*/
+
+
 exports.loadScenario = loadScenario;
 exports.parseTimeStr = parseTimeStr;
 //exports.console = console;
